121b3195b3461a187a6884b892390c25c9a4994f
Aaron de Boer <aaron.deboer@stu.ocsb.ca>
2024-10-09 13:37:40 -0400

    It fucking sucks and can eat shit (YOU FUCKING SUCK)

--- main.js
+++ main.js
@@ -23,9 +23,5 @@ let to_fahrenheit = Number(prompt("Provide temperature in celsius"))
 console.log(`${to_fahrenheit} degrees celsius is ${Math.round((to_fahrenheit*9/5)+32)} degrees fahrenheit.`)
 let to_celsius = Number(prompt("Provide temperature in fahrenheit"))
 console.log(`${to_celsius} degrees fahrenheit is ${Math.round((to_celsius-32)*5/9)} degrees celsius`)
-function roll_d10() {
-    (`You rolled a(n):${(Math.ceil(Math.random()*10))}, neat!`)
-}
-function roll_d6() {
-    (`You rolled a(n): ${(Math.ceil(Math.random()*6))}, neat!`)
-}
+let roll_d10 = console.log(`You rolled a(n):${(Math.ceil(Math.random()*10))}, neat!`)
+let roll_d6 = console.log(`You rolled a(n): ${(Math.ceil(Math.random()*6))}, neat!`)
